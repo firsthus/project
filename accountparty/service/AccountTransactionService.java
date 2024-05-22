b71224168dc59fda84451f5d708e120fc0b2ac61
vohoangxuan <hoangxuanvot@gmail.com>
2024-05-22 13:24:30 -0500

    Commit

--- accountparty/service/AccountTransactionService.java
+++ accountparty/service/AccountTransactionService.java
@@ -5,10 +5,12 @@ import edu.mum.cs.cs525.labs.exercises.project.accountparty.repository.AccountRe
 import edu.mum.cs.cs525.labs.exercises.project.accountparty.rule.RulesEngine;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.List;
 
-public class AccountTransactionService {
+public class AccountTransactionService implements AccountTransactionServiceInterface {
 
     private final AccountRepository accountRepository;
     private final RulesEngine rulesEngine;
@@ -17,7 +19,6 @@ public class AccountTransactionService {
         this.accountRepository = accountRepository;
         this.rulesEngine = rulesEngine;
     }
-
 
     public void deposit(String accountNumber, BigDecimal amount) {
         Account account = accountRepository.findByAccountNumber(accountNumber);
@@ -35,6 +36,19 @@ public class AccountTransactionService {
         Account fromAccount = accountRepository.findByAccountNumber(fromAccountNumber);
         Account toAccount = accountRepository.findByAccountNumber(toAccountNumber);
         fromAccount.transferFunds(toAccount, amount);
+    }
+
+    public String generateReport() {
+        List<Account> accounts = getAccountRepository().getAllAccounts();
+        StringBuilder report = new StringBuilder("Banking Accounts Report:\n");
+        for (Account account : accounts) {
+            report.append(account.generateAccountReport(LocalDate.MIN, LocalDate.MAX)).append("\n");
+        }
+        return report.toString();
+    }
+
+    protected AccountRepository getAccountRepository() {
+        return accountRepository;
     }
 
 }
---
+++ accountparty/service/AccountTransactionServiceInterface.java
@@ -0,0 +1,10 @@
+package edu.mum.cs.cs525.labs.exercises.project.accountparty.service;
+
+import java.math.BigDecimal;
+
+public interface AccountTransactionServiceInterface {
+    void deposit(String accountNumber, BigDecimal amount);
+    void withdraw(String accountNumber, BigDecimal amount);
+    void transferFunds(String fromAccountNumber, String toAccountNumber, BigDecimal amount, String description);
+    String generateReport();
+}
