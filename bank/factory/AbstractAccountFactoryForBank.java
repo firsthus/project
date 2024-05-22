bbe63700d2bb31ae30743814d34e2a8dae8c58bd
basselElkady <33402496+basselElkady@users.noreply.github.com>
2024-05-22 14:56:50 -0500

    update Bank Entity and make the main method for bank entity

--- bank/factory/AbstractAccountFactoryForBank.java
+++
@@ -1,8 +0,0 @@
-package edu.mum.cs.cs525.labs.exercises.project.bank.factory;
-
-import edu.mum.cs.cs525.labs.exercises.project.accountparty.factory.AccountFactory;
-import edu.mum.cs.cs525.labs.exercises.project.accountparty.repository.AccountRepository;
-
-public abstract class AbstractAccountFactoryForBank implements AccountFactory {
-    AccountRepository accountRepository = new AccountRepository();
-}
