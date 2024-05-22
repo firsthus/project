8bfa9cf3ddef74fae232dfe067475066133035f6
firsthus <audufirsthus@gmail.com>
2024-05-22 11:54:59 -0500

    Added account type factory

---
+++ accountparty/factory/AccountTypeFactory.java
@@ -0,0 +1,9 @@
+package edu.mum.cs.cs525.labs.exercises.project.accountparty.factory;
+
+import edu.mum.cs.cs525.labs.exercises.project.accountparty.entity.AccountType;
+import edu.mum.cs.cs525.labs.exercises.project.accountparty.entity.InterestCalculationStrategy;
+
+public interface AccountTypeFactory {
+
+    AccountType provide(InterestCalculationStrategy interestCalculationStrategy);
+}
