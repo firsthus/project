357d6f8501a970fa763668a664efb10b6cbd1423
vohoangxuan <hoangxuanvot@gmail.com>
2024-05-22 22:42:20 -0500

    Add observer and rules to concrete services

--- bank/ServiceLayer/Implmentation/BankAccountService.java
+++ bank/ServiceLayer/Implmentation/BankAccountService.java
@@ -3,11 +3,15 @@ package edu.mum.cs.cs525.labs.exercises.project.bank.ServiceLayer.Implmentation;
 import edu.mum.cs.cs525.labs.exercises.project.accountparty.entity.Account;
 import edu.mum.cs.cs525.labs.exercises.project.accountparty.entity.Customer;
 import edu.mum.cs.cs525.labs.exercises.project.accountparty.factory.AccountTypeFactory;
+import edu.mum.cs.cs525.labs.exercises.project.accountparty.notification.EmailSender;
 import edu.mum.cs.cs525.labs.exercises.project.accountparty.repository.AccountRepository;
+import edu.mum.cs.cs525.labs.exercises.project.accountparty.rule.RulesEngine;
 import edu.mum.cs.cs525.labs.exercises.project.accountparty.service.AccountService;
 import edu.mum.cs.cs525.labs.exercises.project.bank.factory.BankAccountFactory;
 import edu.mum.cs.cs525.labs.exercises.project.bank.factory.CheckingTypeFactory;
 import edu.mum.cs.cs525.labs.exercises.project.bank.factory.SavingTypeFactory;
+import edu.mum.cs.cs525.labs.exercises.project.bank.rule.CompanyAccountEmailRule;
+import edu.mum.cs.cs525.labs.exercises.project.bank.rule.PersonalAccountEmailRule;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -17,6 +21,12 @@ public class BankAccountService extends AccountService {
 
     public BankAccountService(AccountRepository accountRepository) {
         super(accountRepository);
+        RulesEngine rulesEngine = new RulesEngine();
+
+        // Adding rules
+        rulesEngine.addRule(new CompanyAccountEmailRule());
+        rulesEngine.addRule(new PersonalAccountEmailRule());
+        addTransactionObserver(EmailSender.getInstance(rulesEngine));
     }
 
     public Account createAccount(Customer customer, String accountType) {
